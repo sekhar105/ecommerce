d511b49df48b61c8cdb74d6f0a948187ac27c335
sekhar105 <repaka99999@gmail.com>
2025-02-16 21:22:26 +0530

    added comment in feature2

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -29,7 +29,7 @@ public class OrderController {
 	}
 	@DeleteMapping("/delete")
 	public ResponseEntity<String> deleteOrder(@RequestBody OrderCancelDto orderCancelDto){
-		
+		//added commiet in feature 2
 		this.orderServcie.cancelOrder(orderCancelDto);
 		// added new line in line no 34
 		return new ResponseEntity<String>("order deleted successfull",HttpStatus.OK);
